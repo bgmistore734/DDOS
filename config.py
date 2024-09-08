4f955706f8717a076c33eb6313cad7aeac2f0c9a
bgmistore734 <bgmistore734@gmail.com>
2024-09-08 18:04:14 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7317512300:AAExRYZK84_D7792SAiohQP0Q6VcTYRDsHo")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("raj_9479")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_ID=("6337271473")
+
+
+
+
+
+
+
+  
